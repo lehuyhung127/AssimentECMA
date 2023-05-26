ac9535dd576e0835c2afb660edb858fb88d264f7
Lê Huy Hùng <huyhungsp2003@gmail.com>
2023-05-26 10:03:58 +0700

    upadate img1

--- src/dataProjectHomePage.js
+++ src/dataProjectHomePage.js
@@ -1,13 +1,13 @@
 export const dataHomePage = [
-{title: "Project 1", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", img: "../public/image/projects.png"},
-{title: "Project 2", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", img: "../public/image/projects.png"},
-{title: "Project 3", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", img: "../public/image/projects.png"},
-{title: "Project 4", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", img: "../public/image/projects.png"},
+{title: "Project 1", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", img: "./image/projects.png"},
+{title: "Project 2", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", img: "./image/projects.png"},
+{title: "Project 3", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", img: "./image/projects.png"},
+{title: "Project 4", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", img: "./image/projects.png"},
 ];
 
 export const dataService = [
-    {title: "LOGO DESIGN", content: "magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", img: "../public/image/logo-design.png"},
-    {title: "WEBSITE DESIGN", content: "magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", img: "../public/image/website-design.png"},
-    {title: "SUPPORT", content: "magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", img: "../public/image/support.png"},
+    {title: "LOGO DESIGN", content: "magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", img: "./image/logo-design.png"},
+    {title: "WEBSITE DESIGN", content: "magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", img: "./image/website-design.png"},
+    {title: "SUPPORT", content: "magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", img: "./image/support.png"},
     
     ]
